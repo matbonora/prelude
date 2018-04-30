5d95ae3fbcc90598ec10257a54b037d8a367eafd
Matthias Bonora <matthias.bonora@cern.ch>
2018-04-30 11:36:00 +0200

    update config

--- personal/custom.el
+++ personal/custom.el
@@ -3,30 +3,17 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(ansi-color-names-vector
-   ["#031a16" "#3e9688" "#883e96" "#3e4c96" "#96883e" "#4c963e" "#96883e" "#81b5ac"])
- '(ansi-term-color-vector
-   [unspecified "#031a16" "#3e9688" "#883e96" "#3e4c96" "#96883e" "#4c963e" "#96883e" "#81b5ac"])
- '(c-basic-offset 2)
- '(cmake-ide-cmakelists-dir ~/git/ru_simulation/source/model/)
  '(custom-safe-themes
    (quote
-    ("92192ea8f0bf04421f5b245d906701abaa7bb3b0d2b3b14fca2ee5ebb1da38d8" "34ed3e2fa4a1cb2ce7400c7f1a6c8f12931d8021435bad841fdc1192bd1cc7da" "b3bcf1b12ef2a7606c7697d71b934ca0bdd495d52f901e73ce008c4c9825a3aa" "d5b121d69e48e0f2a84c8e4580f0ba230423391a78fcb4001ccb35d02494d79e" default)))
- '(global-linum-mode t)
+    ("9a155066ec746201156bb39f7518c1828a73d67742e11271e4f24b7b178c4710" default)))
  '(package-selected-packages
    (quote
-    (blackboard-theme monokai-alt-theme graphviz-dot-mode company-c-headers auto-complete-clang auto-complete-clang-async cpputils-cmake org-gnome org-projectile org-pomodoro srefactor rtags zop-to-char zenburn-theme writegood-mode which-key volatile-highlights unfill undo-tree tangotango-theme solarized-theme smex smartrep smartparens smart-mode-line rainbow-mode rainbow-delimiters py-autopep8 project-explorer ov operate-on-number move-text monokai-theme markdown-mode magit langtool key-chord json-mode js2-mode imenu-anywhere ido-ubiquitous helm-projectile helm-descbinds helm-ag guru-mode grizzl god-mode gitignore-mode gitconfig-mode git-timemachine gist geiser flycheck flx-ido expand-region emojify elpy elisp-slime-nav ein editorconfig easy-kill discover-my-major diminish diff-hl define-word csv-mode crux company-auctex company-anaconda cmake-project cmake-ide cmake-font-lock clang-format cdlatex browse-kill-ring beacon base16-theme anzu ace-window)))
- '(user-mail-address "matthias.bonora@cern.ch")
- '(vhdl-clock-edge-condition (quote function))
- '(vhdl-company-name "CERN")
+    (markdown-mode leuven-theme cmake-mode auto-complete-clang rtags clang-format py-autopep8 define-word langtool emojify base16-theme tangotango-theme company-anaconda elpy ein org-gnome org-projectile org-pomodoro project-explorer cpputils-cmake zop-to-char zenburn-theme writegood-mode which-key volatile-highlights undo-tree smex smartrep smartparens smart-mode-line rainbow-mode rainbow-delimiters projectile ov operate-on-number move-text magit json-mode js2-mode imenu-anywhere ido-ubiquitous guru-mode grizzl god-mode gitignore-mode gitconfig-mode git-timemachine gist geiser flycheck flx-ido expand-region elisp-slime-nav editorconfig easy-kill discover-my-major diminish diff-hl crux company browse-kill-ring blackboard-theme beacon anzu ace-window)))
  '(vhdl-electric-mode t)
- '(vhdl-platform-spec "Kintex Ultrascale")
- '(vhdl-reset-active-high t)
- '(vhdl-reset-kind (quote sync))
  '(vhdl-stutter-mode t))
 
 
-;(setq prelude-guru nil)
+(setq prelude-guru nil)
 
 (require 'whitespace)
 (setq whitespace-line-column 120) ;; limit line length

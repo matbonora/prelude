0ebe8f5896d042d8396521b4c475200446ea3e86
Matthias Bonora <matthias.bonora@cern.ch>
2021-01-18 17:08:34 +0100

    Test

--- init.el
+++ init.el
@@ -38,6 +38,7 @@
 ;; just comment it out by adding a semicolon to the start of the line.
 ;; You may delete these explanatory comments.
 ;(package-initialize)
+(setq comp-deferred-compilation t)
 
 (defvar current-user
   (getenv
